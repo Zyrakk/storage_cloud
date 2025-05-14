2d138a6de67b33020ee6f4ef1ec7a94ca934804b
Zyrakk <sykblade1@gmail.com>
2025-05-14 12:58:14 +0200

    Update index.php

--- storage-app/public/index.php
+++ storage-app/public/index.php
@@ -1,15 +1,3 @@
 <?php
-echo "<h1>Bienvenido a Storage Stefsec</h1>";
-echo "<p><a href=\"login.php\">Iniciar sesión</a> | <a href=\"register.php\">Registrarse</a></p>";
+die(">>> PHP está funcionando <<<");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    olaaaaaaaaaaaaaaaaaaaaa
-</body>
-</html>
