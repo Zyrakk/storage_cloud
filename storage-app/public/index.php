fedb2e5bc2248673ed4824cec131105d02e42117
Zyrakk <sykblade1@gmail.com>
2025-05-14 12:56:55 +0200

    Update index.php

--- storage-app/public/index.php
+++ storage-app/public/index.php
@@ -1,5 +1,15 @@
 <?php
-// Página de inicio de tu app de storage
 echo "<h1>Bienvenido a Storage Stefsec</h1>";
 echo "<p><a href=\"login.php\">Iniciar sesión</a> | <a href=\"register.php\">Registrarse</a></p>";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    olaaaaaaaaaaaaaaaaaaaaa
+</body>
+</html>
