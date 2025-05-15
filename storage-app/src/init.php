13b3f929896988f118b517947a5d3c04fc152f59
Zyrakk <sykblade1@gmail.com>
2025-05-15 12:57:31 +0200

    Update init.php

--- storage-app/src/init.php
+++ storage-app/src/init.php
@@ -3,7 +3,7 @@
 session_start();
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/src/init.php';
+require __DIR__ . '/config.php';
 
 use Prometheus\CollectorRegistry;
 use Prometheus\RenderTextFormat;
