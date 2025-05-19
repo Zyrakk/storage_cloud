dfb5f33ba426edb5e838496213b93eac1c39ccc3
Zyrakk <sykblade1@gmail.com>
2025-05-19 09:33:13 +0200

    Update Metrics.php

--- storage-app/src/Metrics.php
+++ storage-app/src/Metrics.php
@@ -7,7 +7,7 @@ class Metrics {
     public static function output(): void {
         global $registry;
         $renderer = new RenderTextFormat();
-        header('Content-Type: ' . RenderTextFormat::MIME_TYPE);
+        header('Content-Type: text/plain; version=0.0.4');
         echo $renderer->render($registry->getMetricFamilySamples());
     }
 }
