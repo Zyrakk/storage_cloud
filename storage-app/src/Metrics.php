9c57e989ef7d493133379d3ce90cdddd96654786
Zyrakk <sykblade1@gmail.com>
2025-05-19 09:46:33 +0200

    promphp updated

--- storage-app/src/Metrics.php
+++ storage-app/src/Metrics.php
@@ -7,7 +7,7 @@ class Metrics {
     public static function output(): void {
         global $registry;
         $renderer = new RenderTextFormat();
-        header('Content-Type: text/plain; version=0.0.4');
+        header('Content-Type: ' . RenderTextFormat::MIME_TYPE);
         echo $renderer->render($registry->getMetricFamilySamples());
     }
 }
